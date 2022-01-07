fc57bc06177406938a3faf516efa9f97b58f96d7
ditcas <juditcastro.v@gmail.com>
2022-01-07 11:49:05 +0100

    Modifica exemple teòric de dictionaries i afegeix exercicis de Tema 9 dictionaries

--- dictionaries_exemple.py
+++ dictionaries_exemple.py
@@ -6,24 +6,35 @@ counts = dict()
 line = input("Enter a line of text:")
 # line = line.rstrip() Si la lectura fos d'un text, hauríem de suprimir els intros del final de cada línea.
 line = line.translate(line.maketrans("","",string.punctuation)) #Elimina de la línea els signes de puntuació, perquè sinó, hola! i hola, serien dues paraules diferents.
-# print(line)
 line = line.lower() #Ho posa tot en minúscules, perquè sinó Hola i hola serien paraules diferents.
-# print(line)
 words = line.split()
-# print(words)
 
 for word in words :
     counts[word] = counts.get(word,0) + 1
 
-# print(counts)
-
-sorted()
+# Busquem la paraula més repetida i quantes vegades ho està, és a dir, en el diccionari counts mirem qui té l'ítem valor més gran.
 
 bigcount = None
 bigword = None
 for word,count in counts.items() :
-    if bigcount is None or count > bigcount :
+    if bigcount is None or count > bigcount : # bigcount is None succeeix en la primera iteració, quan és la primera paraula que mirem, aquesta és per defecte la més repetida. Després ja comparem.
         bigcount = count
         bigword = word
 print(bigword, bigcount)
 
+# Manera alternativa. Ordenem el diccionari per l'ítem valor, i ens quedem amb el primer ítem. A més, si el següent ítem té el mateix valor (dos o més paraules igual de repetides), volem mostrar-los tots!
+
+sort_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True) # count.items és una llista de tuples key-value. El resultat d'aquest sorted és una llista de tuples ordenades pel segon ítem, value, de gran a petit (reverse=true)
+
+# sort_counts = sorted([ (value,key) for key,value in counts.items() ], reverse=True) Manera alternativa d'ordenar per valor. Creem una llista de tuples que inverteix les tuples de counts.items i posa primer el valor. Aleshores, sorted ordena aquesta llista pels valors, que és el que volíem. El que passa és que la llista final ordenada està invertida, primer té els valors i després la key.
+
+# Si haguéssim volgut ordenar per key, aleshores és tant fàcil com
+# sort_key_counts = sorted(counts.items())
+
+maxims = list() # Creem una llista on col·locar les paraules més repetides
+
+for tupla in sort_counts :
+    if tupla[1] == sort_counts[0][1] : # Si el valor de la paraula de cada tupla és igual que el valor de la primera paraula del diccionari (que és la més gran perquè l'hem ordenat), l'afegim a la llista de maxims
+        maxims.append(tupla)
+
+print(maxims)
---
+++ ex_09.py
@@ -0,0 +1,40 @@
+# Escribir un programa que clasifica cada mensaje de correodependiendo del día de la semana en que se recibió. Para hacer esto busca las líneas que comienzan con “From”, después busca por la tercer palabra y mantén un contador para cada uno de los días de la semana. Al final del programa imprime los contenidos de tu diccionario (el orden no importa).
+# Escribe un programa para leer a través de un historial de correos, construye un histograma utilizando un diccionario para contar cuántos mensajes han llegado de cada dirección de correo electrónico, e imprime el diccionario.
+# Agrega código al programa anterior para determinar quién tiene la mayoría de mensajes en el archivo.
+# Este programa almacena el nombre del dominio (en vez de la dirección) desde donde fue enviado el mensaje en vez de quién envió el mensaje (es decir, la dirección de correo electrónica completa). Al final del programa, imprime el contenido de tu diccionario.
+
+import sys
+
+file = input("Indica el nom de l'arxiu: ")
+try :
+    file = open(file,"r")
+except :
+    print("No s'ha pogut trobar l'arxiu:", file)
+    sys.exit()
+
+day_counts = dict()
+mail_counts = dict()
+origin_counts = dict()
+
+for line in file :
+    if not line.startswith("From ") : #L'exercici demana incloure només els From_ i no els From:
+        continue
+    words = line.split()
+    mail = words[1].split("@")
+    day_counts[words[2]] = day_counts.get(words[2],0) + 1
+    mail_counts[words[1]] = mail_counts.get(words[1],0) +1
+    origin_counts[mail[1]] = origin_counts.get(mail[1],0) +1
+
+print("Quantitat de mails rebuts per dia de la setmana", day_counts)
+print("Quantitat de mails rebuts per persona", mail_counts)
+print("Quantitat de mails rebuts per organització", origin_counts)
+
+sort_mail_counts = sorted(mail_counts.items(), key=lambda x: x[1], reverse=True)
+
+maxims = list() # Creem una llista on col·locar els mails més repetits
+
+for tupla in sort_mail_counts :
+    if tupla[1] == sort_mail_counts[0][1] : # Si el valor del mail de cada tupla és igual que el valor del primer mail (que és el més gran perquè l'hem ordenat), l'afegim a la llista de maxims
+        maxims.append(tupla)
+
+print("Persona o persones de les quals hem rebut més correus:", maxims)
